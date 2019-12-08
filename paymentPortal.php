25f02477d441c7806505ed1ecf4d99cf3d2b0099
Amlana-P <50160346+Amlana-P@users.noreply.github.com>
2019-12-08 10:03:19 +0530

    Add files via upload

---
+++ paymentPortal.php
@@ -0,0 +1,4 @@
+<?php
+    echo '<span style="text-align: center; color: red;"><h2> You have registered successfully!</h2><h4 style="color: red"> Payment Portal will be live soon.</h4> </span> ';
+    header( "refresh:4;url=wherever.php" );
+?>
